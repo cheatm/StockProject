30959aa5428e2ef4057b3cf21900f9171ff43f4f
caimeng <862786917@qq.com>
2016-08-10 17:35:32 +0800

    HKStock update

--- HKStock.py
+++ HKStock.py
@@ -11,24 +11,30 @@ def setToken(token):
     print(ts.get_token())
 
 def getIndexData(tick):
-    print('Requiring HK market data')
+    print('Requiring index %s' % tick)
     data=mkt.MktIdxd(ticker=tick,field='ticker,tradeDate,openIndex,lowestIndex,highestIndex,closeIndex')
 
     return(data)
+
+def getStockData():
+    print('Requiring stock data')
+    # data=mkt.MktEqud(ticker='600000')
+    data=mkt.MktHKEqud(tradeDate='2016-08-10')
+    return (data)
 
 
 if __name__ == '__main__':
     # setToken('13a8a6f82ca1f297acfc32c92a6c761b9e00de7ca61a0551fb2d0e62676d76d1')
 
+    # for i in HKindex:
+    #
+    #     indexData=getIndexData(i)
+    #
+    #     indexData.to_excel('%s.xlsx' % i)
+    # # indexData=pandas.read_excel('%s.xlsx' % HKindex[1])
+    #
+    #
+    # print(indexData)
 
-
-    for i in HKindex:
-
-        indexData=getIndexData(i)
-
-        indexData.to_excel('%s.xlsx' % i)
-    # indexData=pandas.read_excel('%s.xlsx' % HKindex[1])
-
-
-    print(indexData)
-
+    # print(getStockData())
+    print(ts.get_token())
