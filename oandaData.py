eebac08960e93b3e2d08ee1c9254d2a884fe1fcc
caimeng <862786917@qq.com>
2016-08-23 17:57:15 +0800

    initFile change

--- oandaData.py
+++ oandaData.py
@@ -2,9 +2,10 @@ from pyoanda import Client, TRADE
 import pandas,time,datetime
 import sqlite3
 
-folder='Oanda'
+folder='ini'
 instruments='Instrument.txt'
 granularity=['M15','H1','H4','D','W','M']
+savePath=open('ini/OandaSavePath.txt').read()
 
 defautClient= Client(
             environment=TRADE,
@@ -16,9 +17,27 @@ defautClient= Client(
 def getInstrumentHistory(instrument,candle_format="bidask",granularity='D', count=None,
             daily_alignment=0, alignment_timezone='Etc/UTC',
             weekly_alignment="Monday", start=None,end=None,client=defautClient,recursion=False):
+    '''
+
+    :param instrument:
+    :param candle_format:
+    :param granularity:
+    :param count:
+    :param daily_alignment:
+    :param alignment_timezone:
+    :param weekly_alignment:
+    :param start:
+    :param end:
+    :param client:
+        See more:
+            http://developer.oanda.com/rest-live/rates/#retrieveInstrumentHistory
+    :param recursion: ignore this
+    :return: DataFrame()
+    '''
 
     pdata=None
     print(start)
+
     if recursion:
         data=client.get_instrument_history(
                 instrument=instrument,
@@ -33,8 +52,6 @@ def getInstrumentHistory(instrument,candle_format="bidask",granularity='D', coun
             )
         pdata=pandas.DataFrame(data['candles'])
 
-
-        pass
     else:
         try:
             data=client.get_instrument_history(
@@ -52,6 +69,7 @@ def getInstrumentHistory(instrument,candle_format="bidask",granularity='D', coun
 
         except Exception as e:
             print(e)
+            # 如果所需candle数 > 5000 则从start开始每5000根获取一次并合并数据，recursion=True
             if '5000' in str(e):
                 data=getInstrumentHistory(instrument,candle_format=candle_format,granularity=granularity, count=5000,
                                         daily_alignment=daily_alignment, alignment_timezone=alignment_timezone,
@@ -59,13 +77,9 @@ def getInstrumentHistory(instrument,candle_format="bidask",granularity='D', coun
                 return(data)
             else : return 0
 
-
-
-
     timeSting=[]
-
+    # 修改时间格式
     for i in pdata.index:
-        # 修改时间格式
         t=pdata.get_value(i,'time')
         tSting=t.split('.')[0]
         timeSting.append(tSting)
@@ -89,10 +103,11 @@ def getInstrumentHistory(instrument,candle_format="bidask",granularity='D', coun
     pdata.insert(0,'Date',timeSting)
 
     if recursion:
+        # 如果递归且当前数据=5000组 说明没有读取到结束位置的数据 则继续递归 由当前数据的最后一个作为start输入
 
-        oldendtime=pdata.index.tolist()[-1]
-        startTime=datetime.datetime.fromtimestamp(oldendtime).strftime("%Y-%m-%dT%H:%M:%S.%f%z")
         if len(pdata.index)==5000:
+            oldendtime=pdata.index.tolist()[-1]
+            startTime=datetime.datetime.fromtimestamp(oldendtime).strftime("%Y-%m-%dT%H:%M:%S.%f%z")
 
             new = getInstrumentHistory(instrument,candle_format=candle_format,granularity=granularity, count=5000,
                                     daily_alignment=daily_alignment, alignment_timezone=alignment_timezone,
@@ -103,6 +118,15 @@ def getInstrumentHistory(instrument,candle_format="bidask",granularity='D', coun
     return (pdata)
 
 def update(dbpath,*granularity,instrument=None):
+    '''
+
+    :param dbpath: address to save
+    :param granularity: 'M15','H1','H4','D','W','M'
+    :param instrument: instrument to get
+        If dbpath is named by instrument itself, instrument can be None
+    :return: None
+    '''
+
     con=sqlite3.connect(dbpath)
 
     if instrument is None:
@@ -115,13 +139,12 @@ def update(dbpath,*granularity,instrument=None):
 
     for g in granularity:
         lastRecord=con.execute('''SELECT * FROM "%s" ORDER BY rowid DESC ''' % g).fetchone()
-        startTime=datetime.date.fromtimestamp(lastRecord[0])
-        new=getInstrumentHistory(instrument,granularity=g,start=startTime,end=datetime.date.today())
+        startTime=datetime.datetime.fromtimestamp(lastRecord[0]).strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+        endTime=datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+        new=getInstrumentHistory(instrument,granularity=g,start=startTime,end=endTime)
         new.to_sql(g,con,if_exists='append')
 
-
     con.close()
-
 
 def Split(word,seps):
 
@@ -163,42 +186,25 @@ def readInsts():
 
     return out
 
-def test(instrument,start=None,end=None,count=None,client=defautClient):
-    print(start)
-    data=client.get_instrument_history(instrument, candle_format="midpoint",
-                               granularity='D',count=count,daily_alignment=0,
-                               weekly_alignment="Monday", alignment_timezone='Asia/Hong_Kong',
-                               start=start
-                               )
-
-    return pandas.DataFrame(data['candles'])
-
 
 if __name__ == '__main__':
-    end=datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%z")
-
-    start=datetime.datetime(2001,1,1).strftime("%Y-%m-%dT%H:%M:%S.%f%z")
-
-    granularity=['M15','H1','H4','D','W','M']
+    # end=datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+    #
+    # start=datetime.datetime(2001,1,1).strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+    #
     Insts=readInsts()
-    i=Insts[2]
-    g=granularity[1]
+    #
+    # path='E:/StockProject/Oanda'
+    # for i in Insts:
+    #     dbpath='%s/%s.db' % (path,i)
+    #     print(dbpath)
+    #     con=sqlite3.connect(dbpath)
+    #
+    #     for g in granularity[4:]:
+    #         print(g)
+    #         save_sql(getInstrumentHistory(i,start=start,end=end,candle_format='bidask',granularity=g),g,con=con)
+    #
+    #     con.close()
 
-
-    path='E:/StockProject/Oanda'
-    for i in Insts:
-        dbpath='%s/%s.db' % (path,i)
-        print(dbpath)
-        con=sqlite3.connect(dbpath)
-
-        for g in granularity[4:]:
-            print(g)
-            # path='%s/%s.db' % (folder,i)
-            save_sql(getInstrumentHistory(i,start=start,end=end,candle_format='bidask',granularity=g),g,con=con)
-
-        con.close()
-
-    # path='E:/StockProject/Oanda/GBP_USD.db'
-    # update(path,'H4')
-    # end=datetime.datetime.now()
-
+    print(savePath)
+    update(savePath+Insts[0],'H4')
